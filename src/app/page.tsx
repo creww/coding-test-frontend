122cd70912ef33f2f6fc84d3f30d5bca02a4c0b7
sho-itagaki <s-itagaki@creww.me>
2025-02-05 20:22:55 +0900

    add base

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+export default function Home() {
+  return (
+    <main>
+      <section>
+        <h1>コーディング試験</h1>
+
+        <section>
+          <h2>課題1. タブコンポーネント</h2>
+          {/* ここにタブコンポーネントを追加 */}
+        </section>
+
+        <section>
+          <h2>課題2. ボタンコンポーネント</h2>
+          {/* ここにタブコンポーネントを追加 */}
+        </section>
+
+        <section>
+          <h2>課題3. カルーセルコンポーネント</h2>
+          {/* ここにカルーセルコンポーネントを追加 */}
+        </section>
+      </section>
+    </main>
+  );
+}
